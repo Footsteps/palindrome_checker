bb3f4b0168b71fa722b43e51aa08ddc824f69262
Angela Schumacher <angelaschumacher@web.de>
2024-12-03 13:30:39 +0100

    html and css

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("i am here!!");
+const checkButton = document.getElementById("check-btn");
+const textInput = document.getElementById("text-input");
+
+//event listeners
+checkButton.addEventListener("click", () => {
+    const inputValue = textInput.value;
+    //alert if there is no input
+    if(inputValue.value == "") {
+        alert("Please input a value");
+    } 
+    
+  });
