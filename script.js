5d2a03f3f09ea18d054dcab9290a17cc3efb303f
Angela Schumacher <angelaschumacher@web.de>
2024-12-04 11:58:50 +0100

    finished palindrome logic

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
 const checkButton = document.getElementById("check-btn");
 const textInput = document.getElementById("text-input");
+const result = document.getElementById("result");
 
-//event listeners
+//event listener
 checkButton.addEventListener("click", () => {
     const inputValue = textInput.value;
     //alert if there is no input
-    if(inputValue.value == "") {
+    if(inputValue === "") {
         alert("Please input a value");
-    } 
-    
+        return;
+    } else {
+        checkIfPalindrome(inputValue);
+        textInput.value = "";
+    }  
   });
+
+  const checkIfPalindrome = (inputValue) => {
+    //remove non alphanumerical characters & toLowerCase
+    const cleanInput = inputValue.replace(/[^A-Za-z0-9]/gi, "").toLowerCase();
+    //reverse input
+    const reversedInput = cleanInput.split("").reverse().join("");
+    const resultMsg = `${inputValue} ${
+    cleanInput == reversedInput ? `is` : `is not` 
+    } a palindrome.`
+    result.innerText = resultMsg;
+}
